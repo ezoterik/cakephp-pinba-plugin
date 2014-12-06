28068c0bbfdda56aec2d557ea5d96b69b2d308cc
Oleg Balykin <ezoterik.h@gmail.com>
2014-12-06 15:33:14 +0200

    Add ORM timer

---
+++ Model/Behavior/TimedBehavior.php
@@ -0,0 +1,97 @@
+<?php
+
+/**
+ * Class TimedBehavior
+ */
+class TimedBehavior extends ModelBehavior {
+
+/**
+ * Behavior settings
+ *
+ * @var array
+ */
+	public $settings = array();
+
+/**
+ * Default setting values
+ *
+ * @var array
+ */
+	protected $_defaults = array();
+
+	/** @var Stopwatch|null */
+	protected $_stopwatch = null;
+
+	/** @var StopwatchEvent|null */
+	protected $_stopwatchEventFind = null;
+
+	/** @var StopwatchEvent|null */
+	protected $_stopwatchEventSave = null;
+
+	/** @var string */
+	protected $_serverName = 'localhost';
+
+/**
+ * {@inheritDoc}
+ */
+	public function setup(Model $Model, $settings = null) {
+		if (is_array($settings)) {
+			$this->settings[$Model->alias] = array_merge($this->_defaults, $settings);
+		} else {
+			$this->settings[$Model->alias] = $this->_defaults;
+		}
+
+		$this->_stopwatch = new Stopwatch();
+		$dsConfig = $Model->getDataSource()->config;
+		if (isset($dsConfig['host'])) {
+			$this->_serverName = $dsConfig['host'];
+			if (isset($dsConfig['port'])) {
+				$this->_serverName .= ':' . $dsConfig['port'];
+			}
+		}
+	}
+
+/**
+ * {@inheritDoc}
+ */
+	public function beforeFind(Model $Model, $queryData) {
+		$this->_stopwatchEventFind = $this->_stopwatch->start(array(
+			'server' => $this->_serverName,
+			'group' => 'orm::find',
+			'model' => $Model->alias,
+		));
+		return true;
+	}
+
+/**
+ * {@inheritDoc}
+ */
+	public function afterFind(Model $Model, $results, $primary = false) {
+		if ($this->_stopwatchEventFind) {
+			$this->_stopwatchEventFind->stop();
+		}
+		return true;
+	}
+
+/**
+ * {@inheritDoc}
+ */
+	public function beforeSave(Model $Model, $options = array()) {
+		$this->_stopwatchEventSave = $this->_stopwatch->start(array(
+			'server' => $this->_serverName,
+			'group' => 'orm::save',
+			'model' => $Model->alias,
+		));
+		return true;
+	}
+
+/**
+ * {@inheritDoc}
+ */
+	public function afterSave(Model $Model, $created, $options = array()) {
+		if ($this->_stopwatchEventSave) {
+			$this->_stopwatchEventSave->stop();
+		}
+		return true;
+	}
+}
---
+++ Model/TimerModel.php
@@ -0,0 +1,136 @@
+<?php
+App::uses('Model', 'Model');
+
+class TimerModel extends Model {
+
+	/** @var Stopwatch|null */
+	protected $_stopwatch = null;
+
+	/** @var string */
+	protected $_serverName = 'localhost';
+
+/**
+ * {@inheritDoc}
+ */
+	public function __construct($id = false, $table = null, $ds = null) {
+		parent::__construct($id, $table, $ds);
+
+		$this->_stopwatch = new Stopwatch();
+		$dsConfig = $this->getDataSource()->config;
+		if (isset($dsConfig['host'])) {
+			$this->_serverName = $dsConfig['host'];
+			if (isset($dsConfig['port'])) {
+				$this->_serverName .= ':' . $dsConfig['port'];
+			}
+		}
+	}
+
+/**
+ * Timer start
+ *
+ * @param string $methodName Method name
+ * @return StopwatchEvent
+ */
+	protected function _getStopwatchEvent($methodName) {
+		$tags = array(
+			'group' => 'orm::' . $methodName,
+			'model' => $this->alias
+		);
+
+		if ($this->_serverName) {
+			$tags['server'] = $this->_serverName;
+		}
+
+		return $this->_stopwatch->start($tags);
+	}
+
+/**
+ * {@inheritDoc}
+ */
+	public function find($type = 'first', $query = array()) {
+		$e = null;
+		if ($this->_stopwatch) {
+			$e = $this->_getStopwatchEvent('find');
+		}
+
+		$result = parent::find($type, $query);
+
+		if ($e !== null) {
+			$e->stop();
+		}
+
+		return $result;
+	}
+
+/**
+ * {@inheritDoc}
+ */
+	public function save($data = null, $validate = true, $fieldList = array()) {
+		$e = null;
+		if ($this->_stopwatch) {
+			$e = $this->_getStopwatchEvent('save');
+		}
+
+		$result = parent::save($data, $validate, $fieldList);
+
+		if ($e !== null) {
+			$e->stop();
+		}
+
+		return $result;
+	}
+
+/**
+ * {@inheritDoc}
+ */
+	public function updateAll($fields, $conditions = true) {
+		$e = null;
+		if ($this->_stopwatch) {
+			$e = $this->_getStopwatchEvent('updateAll');
+		}
+
+		$result = parent::updateAll($fields, $conditions);
+
+		if ($e !== null) {
+			$e->stop();
+		}
+
+		return $result;
+	}
+
+/**
+ * {@inheritDoc}
+ */
+	public function delete($id = null, $cascade = true) {
+		$e = null;
+		if ($this->_stopwatch) {
+			$e = $this->_getStopwatchEvent('delete');
+		}
+
+		$result = parent::delete($id, $cascade);
+
+		if ($e !== null) {
+			$e->stop();
+		}
+
+		return $result;
+	}
+
+/**
+ * {@inheritDoc}
+ */
+	public function query($sql) {
+		$e = null;
+		if ($this->_stopwatch) {
+			$e = $this->_getStopwatchEvent('query');
+		}
+
+		$result = parent::query($sql);
+
+		if ($e !== null) {
+			$e->stop();
+		}
+
+		return $result;
+	}
+}
